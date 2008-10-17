eadf9e6833078f6e49d05da4b932a2fb90ab7dd6
ritzmo <ritzmo@a295cab0-163a-0410-857a-8207b47ee9d6>
2008-10-17 20:14:28 +0000

    make fuzzydateformatter a little easier and round interval up (by adding 0.9 before casting to int o0)
    git-svn-id: svn://svn.freaque.net/enigma2@513 a295cab0-163a-0410-857a-8207b47ee9d6

--- Classes/FuzzyDateFormatter.m
+++ Classes/FuzzyDateFormatter.m
@@ -15,44 +15,45 @@
 // XXX: Ok, this sucks - but the iphone sdk lacks a better way I know about :D
 - (NSString *)stringForObjectValue:(id)date
 {
-	if([self dateStyle] == NSDateFormatterNoStyle)
+	NSDateFormatterStyle dateStyle = [self dateStyle];
+	if(dateStyle == NSDateFormatterNoStyle)
 		return [super stringForObjectValue:date];
 
-	NSDate *thisNight = [NSDate dateWithTimeIntervalSinceNow: -(((long)[NSDate timeIntervalSinceReferenceDate]) + [[self timeZone] secondsFromGMT]) % ONEDAY];
+	NSDate *thisNight = [NSDate dateWithTimeIntervalSinceNow: -((NSInteger)[NSDate timeIntervalSinceReferenceDate] + [[self timeZone] secondsFromGMT]) % ONEDAY];
 
-	NSTimeInterval secSinceToday = [date timeIntervalSinceDate: thisNight];
-	if (secSinceToday > 0 && secSinceToday < ONEDAY)
+	NSInteger secSinceToday = (NSInteger)([date timeIntervalSinceDate: thisNight]+0.9);
+	if (secSinceToday >= 0 && secSinceToday < ONEDAY)
 	{
 		if([self timeStyle] == NSDateFormatterNoStyle)
 			return NSLocalizedString(@"Today", @"");
-		NSDateFormatterStyle tempStyle = [self dateStyle];
+
 		[self setDateStyle: NSDateFormatterNoStyle];
 		NSString *retVal = [NSString stringWithFormat: @"%@, %@", NSLocalizedString(@"Today", @""), [super stringForObjectValue: date]];
-		[self setDateStyle: tempStyle];
+		[self setDateStyle: dateStyle];
 		return retVal;
 	}
 
-	NSTimeInterval secSinceTomorrow = [date timeIntervalSinceDate: [thisNight addTimeInterval: ONEDAY]];
-	if (secSinceTomorrow > 0 && secSinceTomorrow < ONEDAY)
+	NSInteger secSinceTomorrow = secSinceToday - ONEDAY;
+	if (secSinceTomorrow >= 0 && secSinceTomorrow < ONEDAY)
 	{
 		if([self timeStyle] == NSDateFormatterNoStyle)
 			return NSLocalizedString(@"Tomorrow", @"");
-		NSDateFormatterStyle tempStyle = [self dateStyle];
+		
 		[self setDateStyle: NSDateFormatterNoStyle];
 		NSString *retVal = [NSString stringWithFormat: @"%@, %@", NSLocalizedString(@"Tomorrow", @""), [super stringForObjectValue: date]];
-		[self setDateStyle: tempStyle];
+		[self setDateStyle: dateStyle];
 		return retVal;
 	}
 
-	NSTimeInterval secSinceYesterday = [date timeIntervalSinceDate: [thisNight addTimeInterval: -ONEDAY]];
-	if (secSinceYesterday > 0 && secSinceYesterday < ONEDAY)
+	NSInteger secSinceYesterday = secSinceToday + ONEDAY;
+	if (secSinceYesterday >= 0 && secSinceYesterday < ONEDAY)
 	{
 		if([self timeStyle] == NSDateFormatterNoStyle)
 			return NSLocalizedString(@"Yesterday", @"");
-		NSDateFormatterStyle tempStyle = [self dateStyle];
+
 		[self setDateStyle: NSDateFormatterNoStyle];
 		NSString *retVal = [NSString stringWithFormat: @"%@, %@", NSLocalizedString(@"Yesterday", @""), [super stringForObjectValue: date]];
-		[self setDateStyle: tempStyle];
+		[self setDateStyle: dateStyle];
 		return retVal;
 	}
 
