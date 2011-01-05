c6edfd92ccaa184423c60cb5e6572a0db3807f7d
Moritz Venn <moritz.venn@freaque.net>
2011-01-05 01:00:23 +0100

    EGORefreshTableHeaderView.m: use clear background

--- Classes/EGOTableViewPullRefresh/EGORefreshTableHeaderView.m
+++ Classes/EGOTableViewPullRefresh/EGORefreshTableHeaderView.m
@@ -44,7 +44,7 @@
     if (self = [super initWithFrame:frame]) {
 		
 		self.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-		self.backgroundColor = [UIColor colorWithRed:226.0/255.0 green:231.0/255.0 blue:237.0/255.0 alpha:1.0];
+		self.backgroundColor = [UIColor clearColor];
 
 		UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, frame.size.height - 30.0f, self.frame.size.width, 20.0f)];
 		label.autoresizingMask = UIViewAutoresizingFlexibleWidth;
