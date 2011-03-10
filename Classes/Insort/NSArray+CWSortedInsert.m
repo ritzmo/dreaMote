2e054bdd305a55607e610c8db6c39b2116133771
Moritz Venn <moritz.venn@freaque.net>
2011-03-10 12:46:57 +0100

    NSArray+CWSortedInsert.m: fix compiler warning when compiling with llvm

--- Classes/Insort/NSArray+CWSortedInsert.m
+++ Classes/Insort/NSArray+CWSortedInsert.m
@@ -28,7 +28,7 @@
 //  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import "NSArray+CWSortedInsert.h"
-#import <objc/runtime.h>
+#import <objc/message.h>
 
 @implementation NSArray (CWSortedInsert)
 
