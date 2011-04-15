fced0e5916d0ccb492eee6ccccacee2ec85d1904
Moritz Venn <moritz.venn@freaque.net>
2011-04-16 00:56:55 +0200

    EPGRefreshViewController.m: give text fields a fixed width
    improves layout on both ipad and iphone as there are no more clipping
    problems and all fields are in line (well, maybe one or two pixels off).

--- Classes/EPGRefreshViewController.m
+++ Classes/EPGRefreshViewController.m
@@ -647,6 +647,7 @@ enum generalSectionItems
 						_delayCell.view = _delay;
 						_delayCell.textLabel.font = [UIFont systemFontOfSize:kTextViewFontSize];
 						_delayCell.textLabel.text = NSLocalizedStringFromTable(@"Delay if busy (min.)", @"EPGRefresh", @"Label for cell 'delay refresh if not in standby (minutes)'");
+						_delayCell.fixedWidth = 100.0f;
 					}
 					cell = _delayCell;
 					break;
@@ -657,6 +658,7 @@ enum generalSectionItems
 						_intervalCell.view = _interval;
 						_intervalCell.textLabel.font = [UIFont systemFontOfSize:kTextViewFontSize];
 						_intervalCell.textLabel.text = NSLocalizedStringFromTable(@"Time on service (min.)", @"EPGRefresh", @"Label for cell 'Time to stay on service (minutes)'");
+						_intervalCell.fixedWidth = 100.0f;
 					}
 					cell = _intervalCell;
 					break;
