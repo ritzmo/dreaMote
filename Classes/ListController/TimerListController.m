d6aacaa4067b691d55b91c6b0c9ebcb3d15251df
Moritz Venn <moritz.venn@freaque.net>
2011-07-14 11:57:50 +0200

    Revert "TimerListController.m: first reload the table, then set editing"
    This reverts commit 3b6e4c04eedd2293e46421e39b2a50601b69c577.
    
    Makes no sense, as there is nothing to reload before editing is actually
    enabled.

--- Classes/ListController/TimerListController.m
+++ Classes/ListController/TimerListController.m
@@ -168,6 +168,9 @@ static const int stateMap[kTimerStateMax] = {kTimerStateRunning, kTimerStatePrep
 /* (un)set editing */
 - (void)setEditing:(BOOL)editing animated:(BOOL)animated
 {
+	[super setEditing: editing animated: animated];
+	[_tableView setEditing: editing animated: animated];
+
 	if(animated)
 	{
 		if(editing)
@@ -183,9 +186,6 @@ static const int stateMap[kTimerStateMax] = {kTimerStateRunning, kTimerStatePrep
 	}
 	else
 		[_tableView reloadData];
-
-	[super setEditing:editing animated:animated];
-	[_tableView setEditing:editing animated:animated];
 }
 
 /* about to appear */
