7c3c8269da299a51b43ac726b41d8de9413b189a
Moritz Venn <moritz.venn@freaque.net>
2011-11-07 18:04:10 +0100

    bin/mergeLocalizable.py: add ServiceEditor to tables

--- bin/mergeLocalizable.py
+++ bin/mergeLocalizable.py
@@ -6,7 +6,7 @@ import re
 import os
 import codecs
 pattern = re.compile('^"(.*?)" = "(.*?)";.*?')
-tables = "Localizable", "AutoTimer", "EPGRefresh", "PackageManager"
+tables = "Localizable", "AutoTimer", "EPGRefresh", "PackageManager", "ServiceEditor"
 
 def find(dirname, recursive, *args):
 	files = []
