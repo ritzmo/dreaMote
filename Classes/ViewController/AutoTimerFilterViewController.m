89e5a961a7e011b908a4484f14709011098d919c
Moritz Venn <moritz.venn@freaque.net>
2011-11-22 08:12:35 +0100

    use the same "safe" pattern for callback everywhere now
    create helper variable and immediately unset instance variable - should
    protect us from most accidents ;)
    
    Conflicts:
    
    	Classes/ListController/SimpleMultiSelectionListController.m
    	Classes/ViewController/TimerViewController.m

--- Classes/ViewController/AutoTimerFilterViewController.m
+++ Classes/ViewController/AutoTimerFilterViewController.m
@@ -155,8 +155,10 @@
 /* cancel */
 - (void)cancelEdit:(id)sender
 {
-	if(callback)
-		callback(NO, nil, filterType, include, oldText, oldInclude);
+	autotimerfilter_callback_t call = callback;
+	callback = nil;
+	if(call)
+		call(NO, nil, filterType, include, oldText, oldInclude);
 }
 
 /* finish */
@@ -174,7 +176,9 @@
 		if([text isEqualToString:@""])
 			text = nil;
 
-		callback(YES, text, filterType, include, oldText, oldInclude);
+		autotimerfilter_callback_t call = callback;
+		callback = nil;
+		call(YES, text, filterType, include, oldText, oldInclude);
 	}
 }
 
--- Classes/ViewController/DatePickerController.m
+++ Classes/ViewController/DatePickerController.m
@@ -42,7 +42,6 @@
 		self.datePickerMode = UIDatePickerModeDateAndTime;
 		self.modalPresentationStyle = UIModalPresentationFormSheet;
 		self.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
-		callback = nil;
 	}
 	
 	return self;
@@ -155,9 +154,7 @@
 /* rotate with device on ipad, otherwise to portrait */
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-	if(IS_IPAD())
-		return YES;
-	return UIInterfaceOrientationIsPortrait(interfaceOrientation);
+	return IS_IPAD() || UIInterfaceOrientationIsPortrait(interfaceOrientation);
 }
 
 /* finish */
@@ -172,7 +169,9 @@
 			NSDateComponents *components = [gregorian components:(NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit) fromDate:date];
 			date = [gregorian dateFromComponents:components];
 		}
-		callback(date);
+		datepicker_callback_t call = callback;
+		callback = nil;
+		call(date);
 	}
 
 	if(IS_IPAD())
